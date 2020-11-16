d35061905a95bfd425f60d70ce97c5f7f821f052
ZypherIsNotZephyr <72026150+ZypherIsNotZephyr@users.noreply.github.com>
2020-11-16 13:45:01 +0800

    4-1

---
+++ nalagr.m
@@ -0,0 +1,12 @@
+function yy=nalagr(x,y,xx)
+m=length(x);n=length(y);
+if m~=n,error('向量x与y的长度必须一致');end
+s=0;
+for i = 1:n
+    t=ones(1,length(xx));
+    for j=[1:i-1,i+1:n]
+        t=t.*(xx-x(j))/(x(i)-x(j));
+    end
+    s=s+t*y(i);
+end
+yy=s;
